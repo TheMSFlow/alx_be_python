02396f807b3515f33d928b1f523eaa162f7ca11c
Michael <themichaelsteve@gmail.com>
2025-06-01 00:32:54 +0100

    2. Multiplication Table Generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
-multiply_by = range(0,11,1)
-for num in multiply_by:
+for num in range(1, 11):
     result = num * number
     print(f"{number} * {num} = {result}")
